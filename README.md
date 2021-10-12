4af28f751d7526cb960ce965bc0600f95565b4a2
Samuel Cristobal <sc@databeacon.aero>
2021-10-12 11:22:33 +0200

    consumer and producer

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # rxkfk
+
 A no fuss rxjs wrapper for kafkajs, focused on ease of use.
+
+## Getting started
+
+> This section follows the same example as in KakfaJS docs (https://kafka.js.org/docs/getting-started)
+
+Install rxkfk using yarn:
+
+```bash
+yarn add rxkfk
+```
+
+Or npm:
+
+```bash
+npm install rxkfk
+```
+
+Let's start by creating our RxJS subjects, using our Kafka brokers, topic and consumer details:
+
+```typescript
+import fromKafkaTopic from 'rxkfk';
+import { of, first } from 'rxjs';
+
+const { message$$, pushMessage$$ } = fromKafkaTopic(
+    {
+        clientId: 'my-app',
+        brokers: ['kafka1:9092', 'kafka2:9092']
+    },
+    { topic: 'test-topic', fromBeginning: true },
+    { groupId: 'test-group' }
+);
+```
+
+Now to produce a message to a topic, we'll subscribe `pushMessage$$` to an observable:
+
+```typescript
+of('Hello KafkaJS user!').subscribe(pushMessage$$);
+```
+
+Finally, to verify that our message has indeed been produced to the topic, let's subscribe an observer to the `message$$` subject:
+
+```typescript
+message$$.pipe(first()).subscribe(console.log);
+```
+
+Congratulations, you just produced and consumed your first Kafka message using RxJS and a bit of help from rxkfk!
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,118 @@
-import { ConsumerConfig, ConsumerSubscribeTopic, EachBatchPayload, Kafka, KafkaConfig } from 'kafkajs';
-import { from, map, mergeAll, Observable, share } from 'rxjs';
+import {
+    ConsumerConfig,
+    ConsumerSubscribeTopic,
+    EachBatchPayload,
+    Kafka,
+    KafkaConfig,
+    ProducerConfig,
+    ProducerRecord
+} from 'kafkajs';
+import { asyncScheduler, from, map, mergeAll, Observable, observeOn, Observer, share, Subject } from 'rxjs';
 
 type JSONObject = { [key: string]: JSON };
 type JSONArray = Array<JSON>;
 type JSON = null | string | number | boolean | JSONArray | JSONObject;
 
-const fromKafkaTopic = function (
-    options: KafkaConfig,
-    topic: ConsumerSubscribeTopic,
-    consumerOptions?: ConsumerConfig
+const rxkfk = function (
+    kafkaOptions: KafkaConfig,
+    topicOptions: ConsumerSubscribeTopic | string,
+    consumerOptions?: ConsumerConfig,
+    producerOptions?: ProducerConfig
 ) {
-    const kafka = new Kafka(options);
+    const kafka = new Kafka(kafkaOptions);
 
-    const consumer = kafka.consumer(consumerOptions);
-
-    const setup = async () => {
-        await consumer.connect();
-
-        await consumer.subscribe(topic);
-    };
-
-    const processBatchWith = async (batchProcessor: (_: EachBatchPayload) => Promise<void>) => {
-        await consumer.run({
-            eachBatchAutoResolve: true,
-            eachBatch: batchProcessor
-        });
-    };
-
-    async function disconnect() {
-        await consumer.disconnect();
+    function isConsumerSubscribeTopic(
+        topicOptions: ConsumerSubscribeTopic | string
+    ): topicOptions is ConsumerSubscribeTopic {
+        return Object.hasOwnProperty.call(topicOptions, 'topic');
     }
 
-    function connect() {
-        const batche$ = new Observable<EachBatchPayload>((subscriber) => {
-            const asyncNext = async (bachBlock: EachBatchPayload) => {
-                subscriber.next(bachBlock);
-            };
+    function assertTopicString(topic: string | RegExp): asserts topic is string {
+        if (!(typeof topic === 'string' || topic instanceof String))
+            throw new Error("Can't send messages to a topic defined by a regex");
+    }
 
-            setup().then(() => processBatchWith(asyncNext));
+    const topic = isConsumerSubscribeTopic(topicOptions) ? topicOptions : { topic: topicOptions };
 
-            return () => {
-                disconnect();
-            };
-        });
+    const batche$ = new Observable<EachBatchPayload>((subscriber) => {
+        const consumer = kafka.consumer(consumerOptions);
 
-        const message$ = batche$.pipe(
-            map((bachBlock) => from(bachBlock.batch.messages)),
-            mergeAll(),
-            map((msg) => {
-                const msgContent = msg.value?.toString();
-                if (!msgContent) return;
-                try {
-                    return JSON.parse(msgContent) as JSON;
-                } catch {
-                    throw new Error('Could not parse message');
+        const processBatchWith = async () => {
+            await consumer.connect();
+
+            await consumer.subscribe(topic);
+
+            await consumer.run({
+                eachBatchAutoResolve: true,
+                eachBatch: async (bachBlock: EachBatchPayload) => {
+                    subscriber.next(bachBlock);
                 }
-            })
-        );
+            });
+        };
 
-        return message$.pipe(share());
-    }
+        processBatchWith();
 
-    return connect();
+        return async () => {
+            await consumer.disconnect();
+        };
+    });
+
+    const message$ = batche$.pipe(
+        map((bachBlock) => from(bachBlock.batch.messages)),
+        mergeAll(),
+        map((msg) => {
+            const msgContent = msg.value?.toString();
+            if (!msgContent) return;
+            try {
+                return JSON.parse(msgContent) as JSON;
+            } catch {
+                throw new Error('Could not parse message');
+            }
+        })
+    );
+
+    const message$$ = message$.pipe(share());
+
+    const pushMessage$$ = new Subject<JSON | undefined>();
+
+    const pushMessage = function () {
+        const producer = kafka.producer(producerOptions); //as Producer & { connected: boolean };
+        let connected = false;
+
+        const topicExp = topic.topic;
+
+        assertTopicString(topicExp);
+
+        return {
+            next: async (message) => {
+                const record: ProducerRecord = {
+                    topic: topicExp,
+                    messages: [{ value: JSON.stringify(message) }]
+                };
+
+                const asyncPush = async (record: ProducerRecord) => {
+                    if (!connected) {
+                        await producer.connect();
+                        connected = true;
+                    }
+
+                    await producer.send(record);
+                };
+
+                await asyncPush(record);
+            },
+            error: (error) => console.error(error),
+
+            complete: async () => {
+                await producer.disconnect();
+                connected = false;
+            }
+        } as Observer<JSON | undefined>;
+    }.call(undefined);
+
+    pushMessage$$.pipe(observeOn(asyncScheduler)).subscribe(pushMessage);
+
+    return { message$$, pushMessage$$ };
 };
 
-export { fromKafkaTopic };
+export default rxkfk;
